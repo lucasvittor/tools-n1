4f2806a611be39b85aef982bab4d1ab51848e7be
lucasvittor <dev.lucssvitor@gmail.com>
2025-05-19 00:24:54 -0300

    new file:   executavel.bat renamed:    tools.ps1 -> orig/tools.ps1

---
+++ executavel.bat
@@ -0,0 +1,2 @@
+@echo off
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"C:\Users\L7CAS\Documents\PROJETOS\SHELL\WINDOWS\tools-n1\test\test.ps1\"' -Verb RunAs"
